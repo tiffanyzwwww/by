ecd9baa7ac2ea9f30d0359b6b431a0f1e069b00e
Leo <leochan2017@gmail.com>
2017-03-27 23:55:03 +0800

    组件化头部

--- js/main.js
+++ js/main.js
@@ -4,22 +4,34 @@ window.tData = {
     imageList: [{
         imgId: '0',
         title: '别浪猥琐发育',
-        src: './images/test.jpg'
+        src: './images/show1.jpg'
     }, {
         imgId: '1',
         title: '稳住我们能赢',
-        src: './images/test.jpg'
+        src: './images/show2.jpg'
     }, {
         imgId: '2',
         title: '好好补兵',
-        src: './images/test.jpg'
+        src: './images/show1.jpg'
     }, {
         imgId: '3',
         title: '不要想着单杀',
-        src: './images/test.jpg'
+        src: './images/show2.jpg'
     }, {
         imgId: '4',
         title: '上单不给就送',
-        src: './images/test.jpg'
+        src: './images/show1.jpg'
     }]
 }
+
+Vue.component('by-header', {
+    data: function() {
+        return {
+
+        }
+    },
+    ready: function() {
+
+    },
+    template: ('<div class="header"><img src="./images/menus.png" alt=""> BY !</div>')
+});
