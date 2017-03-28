d03720a8bd174ba52d8cac5bbd5b101aeb8061b4
Leo <leochan2017@gmail.com>
2017-03-28 11:07:47 +0800

    完成点赞，首页，diy，size

--- js/main.js
+++ js/main.js
@@ -3,23 +3,38 @@ window.tData = {
     isLogin: false,
     imageList: [{
         imgId: '0',
+        author: 'Leo',
+        isLike: false,
+        authorAvatar: './images/user/userimg1.jpg',
         title: '别浪猥琐发育',
         src: './images/show1.jpg'
     }, {
         imgId: '1',
+        author: 'Tiffany',
+        isLike: false,
+        authorAvatar: './images/user/userimg.jpg',
         title: '稳住我们能赢',
         src: './images/show2.jpg'
     }, {
         imgId: '2',
-        title: '好好补兵',
+        author: '陈老师',
+        isLike: false,
+        authorAvatar: './images/user/edc.jpg',
+        title: '上单不给就送',
         src: './images/show1.jpg'
     }, {
         imgId: '3',
+        author: '马云',
+        isLike: false,
+        authorAvatar: './images/user/mayun.jpg',
         title: '不要想着单杀',
         src: './images/show2.jpg'
     }, {
         imgId: '4',
-        title: '上单不给就送',
+        author: 'Pony',
+        isLike: false,
+        authorAvatar: './images/user/mahuateng.jpg',
+        title: '好好补兵',
         src: './images/show1.jpg'
     }]
 }
